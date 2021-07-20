65723c3d7dfa18c5ec5369db86b1695d03625cd3
Ricardo <metrpg@gmail.com>
2021-07-19 21:26:29 -0500

    model y data

---
+++ Client/Data/ClientContext.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppClient.Models;
+
+    public class ClientContext : DbContext
+    {
+        public ClientContext (DbContextOptions<ClientContext> options)
+            : base(options)
+        {
+
+        // creacion de BD automatica
+
+        //  Database.EnsureDeleted(); // elimina la BD
+
+
+
+       Database.EnsureCreated(); // solucion a error Crea BD si no existe
+
+        // si hay cambios en Modelo Reconstruye la  BD - Desarrollo
+        // produccion usar migrate
+
+
+    }
+
+    public DbSet<AppClient.Models.Client> Client { get; set; }
+    }
---
+++ Client/Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace AppClient.Models
+{
+    public class Client
+    {
+
+        // atributos
+
+        public Guid ClientId { get; set; }
+        public string TipDocument { get; set; }
+        public string Document { get; set; }
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public string LName { get; set; }
+        
+
+        public Client()
+        {
+        }
+    }
+}
